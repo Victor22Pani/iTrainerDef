84d378442929be97eaece2d6bdcca82ad9ef6a2b
Victor22Pani <33956013+Victor22Pani@users.noreply.github.com>
2017-11-26 11:43:51 +0100

    Update Target.java

--- app/src/main/java/com/example/victor/itrainerdef/Data/Target.java
+++ app/src/main/java/com/example/victor/itrainerdef/Data/Target.java
@@ -4,12 +4,28 @@ package com.example.victor.itrainerdef.Data;
  * Created by Victor on 26/11/2017.
  */
 
-public class Target {
+public class Card {
     private String minute;
     private boolean color;
 
-    public Target(String m){
+    public Card (String m){
         this.minute=m;
         this.color=false;// si es false amarilla si es true roja
             }
+     public String getMinute() {
+        return minute;
+    }
+
+    public void setMinute(String minute) {
+        this.minute = minute;
+    }
+
+    public boolean isColor() {
+        return color;
+    }
+
+    public void setColor(boolean color) {
+        this.color = color;
+    }
+
 }
